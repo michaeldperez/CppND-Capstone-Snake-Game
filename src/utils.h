e3775636d7d930670673d833544cbafe6507cea2
Michael Perez <perez.michael.d@gmail.com>
2020-10-10 18:43:15 -0700

    Add iostream to utils

--- src/utils.h
+++ src/utils.h
@@ -1,3 +1,7 @@
+#ifndef UTILS_H
+#define UTILS_H
+#include <iostream>
+
 namespace Utils
 {
     enum class Difficulty
@@ -6,4 +10,19 @@ namespace Utils
         MEDIUM,
         HARD
     };
+
+    std::size_t determine_frame_rate(Difficulty difficulty_level)
+    {
+        switch (difficulty_level)
+        {
+        case Difficulty::MEDIUM:
+            return 65;
+        case Difficulty::HARD:
+            return 70;
+        default:
+            return 60;
+        }
+    }
 }
+
+#endif
